516fe96a473fd8e255c6f40830965a6057c229a6
Zainab <zainabturkmen247@gmail.com>
2024-01-02 15:31:24 +0430

    init

--- before/script.js
+++ before/script.js
@@ -1,16 +1,18 @@
 
 const newItemForm = document.getElementById("new-item-form")
-const itemInput = document.getAnimations("item-input")
-const list = document.getAnimations("list")
+const itemInput = document.getElementById("item-input")
+const list = document.getElementById("list")
 
 newItemForm.addEventListener("submit", e  => {
      e.preventDefault()
 
      const newItem = document.createElement('div')
-     newItem.innerText = itemInput.value 
      newItem.classList.add("list-item")
+     newItem.innerText = itemInput.value 
 
      list.appendChild(newItem)
+
+     itemInput.value = ""
 
 
     //  console.log(newItem)
